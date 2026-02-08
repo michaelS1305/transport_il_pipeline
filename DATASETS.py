9e409a4f017b6ff4bd392bb13aebd586ffaab5a2
michaelS1305 <michael231559@gmail.com>
2026-02-08 11:54:35 +0200

    Add files via upload

---
+++ DATASETS.py
@@ -0,0 +1,42 @@
+DATASETS = [
+    {
+        "name": "car_plate_numbers",
+        "api_url": "https://data.gov.il/api/3/action/datastore_search",
+        "resource_id": "053cea08-09bc-40ec-8f7a-156f0677aff3",
+        "catalog": "main",
+        "schema": "bronze",
+        "table": "car_plate_numbers_raw",
+        "load_type": "full",
+        "enabled": True
+    },
+    {
+        "name": "two_wheeled_plate_numbers",
+        "api_url": "https://data.gov.il/api/3/action/datastore_search",
+        "resource_id": "bf9df4e2-d90d-4c0a-a400-19e15af8e95f",
+        "catalog": "main",
+        "schema": "bronze",
+        "table": "two_wheeled_plate_numbers_raw",
+        "load_type": "full",
+        "enabled": True
+    },
+    {
+        "name": "public_transport_plate_numbers",
+        "api_url": "https://data.gov.il/api/3/action/datastore_search",
+        "resource_id": "cf29862d-ca25-4691-84f6-1be60dcb4a1e",
+        "catalog": "main",
+        "schema": "bronze",
+        "table": "public_transport_plate_numbers_raw",
+        "load_type": "full",
+        "enabled": True
+    },
+    {
+        "name": "electric_car_amount_per_region",
+        "api_url": "https://data.gov.il/api/3/action/datastore_search",
+        "resource_id": "07421a4e-5b12-4444-9173-5ca297b31f79",
+        "catalog": "main",
+        "schema": "bronze",
+        "table": "electric_car_amount_per_region_raw",
+        "load_type": "full",
+        "enabled": True
+    }
+]
